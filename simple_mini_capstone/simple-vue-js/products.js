bfef1ff6886fd0153631afe833a886f8db543dc7
Mehmet <mehmetk6158@gmail.com>
2021-11-08 18:45:09 -0500

    post request on vue.js

--- simple_mini_capstone/simple-vue-js/products.js
+++ simple_mini_capstone/simple-vue-js/products.js
@@ -16,21 +16,21 @@ var app = new Vue({
         this.products = response.data;
       });
     },
-  },
-  createTodo: function () {
-    console.log("creating product...");
-    // make post request
-    axios
-      .post("http://localhost:3000/products", {
-        name: "IZZE",
-        description: "it is nice drink",
-        price: 2.99,
-        image_url:
-          "https://cdn.shopify.com/s/files/1/0021/5436/4983/files/izze-home-about3-picnicTub-933x700_600x.jpg?v=1535478579",
-      })
-      .then((response) => {
-        console.log(response.data);
-        this.products.push(response.data);
-      });
+    createProduct: function () {
+      console.log("creating product...");
+      // make post request
+      axios
+        .post("http://localhost:3000/products", {
+          name: "IZZE",
+          description: "it is nice drink",
+          price: 2.99,
+          image_url:
+            "https://cdn.shopify.com/s/files/1/0021/5436/4983/files/izze-home-about3-picnicTub-933x700_600x.jpg?v=1535478579",
+        })
+        .then((response) => {
+          console.log(response.data);
+          this.products.push(response.data);
+        });
+    },
   },
 });
