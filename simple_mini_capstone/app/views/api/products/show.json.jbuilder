c348eb1399a9c0a80f088d93a545db64e7473498
Mehmet <mehmetk6158@gmail.com>
2021-11-08 18:51:48 -0500

    post request on vue.js

--- simple_mini_capstone/app/views/api/products/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.partial! @product, partial: 'product', as: :product
--- simple_mini_capstone/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  get "/products" => "api/products#index"
-  post "/products" => "api/products#create"
-  get "/products/:id" => "api/products#show"
-  patch "/products/:id" => "api/products#update"
-  delete "/products/:id" => "api/products#destroy"
-end
