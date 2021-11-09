2071828d5084915fe5c4acb9e89b924fc22dfa0d
Mehmet <mehmetk6158@gmail.com>
2021-11-08 22:32:35 -0500

    add babel

--- modern_javascript/index.js
+++ modern_javascript/index.js
@@ -2,5 +2,11 @@
 var moment = require("moment");
 var math = require("mathjs");
 
-console.log(sqrt(4).toString());
+console.log(console.log(math.sqrt(4).toString()));
 console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+console.log("hello, my name is bob!");
+
+var name = "Bob",
+  time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
