b9ad804f34c4af4c1b500478e1581695d0862513
Mehmet <mehmetk6158@gmail.com>
2021-11-11 15:21:55 -0500

    add show method

--- simple-contacts-vue-app/src/views/Home.vue
+++ simple-contacts-vue-app/src/views/Home.vue
@@ -22,7 +22,11 @@
       <input type="text" v-model="newContact.image" />
     </p>
     <button v-on:click="createContact">Create Contact</button>
+    <hr />
+    <hr />
+    <h1>Contact List</h1>
     <div v-for="contact in contacts" :key="contact.id">
+      <!-- <img v-bind:src="contact.image" v-bind:alt="contact.first_name" /> -->
       <p>
         <b>Id:</b>
         {{ contact.id }}
@@ -31,8 +35,41 @@
         <b>Name:</b>
         {{ contact.first_name }} {{ contact.last_name }}
       </p>
+      <p><button v-on:click="showContact(contact)">More Info</button></p>
       <hr />
     </div>
+    <dialog id="show-contact">
+      <form method="dialog">
+        <p>
+          <b>First Name:</b>
+          {{ currentContact.first_name }}
+          <!-- <input type="text" v-model="currentContact.first_name" /> -->
+        </p>
+        <p>
+          <b>Last Name:</b>
+          {{ currentContact.last_name }}
+          <!-- <input type="text" v-model="currentContact.last_name" /> -->
+        </p>
+        <p>
+          <b>Email:</b>
+          {{ currentContact.email }}
+          <!-- <input type="text" v-model="currentContact.eamil" /> -->
+        </p>
+        <p>
+          <b>Phone Number:</b>
+          {{ currentContact.phone_number }}
+          <!-- <input type="text" v-model="currentContact.phone_number" /> -->
+        </p>
+        <p>
+          <b>Image:</b>
+          <!-- {{ currentContact.image }} -->
+          <img v-bind:src="currentContact.image" v-bind:alt="currentContact.first_name" />
+
+          <!-- <input type="text" v-model="currentContact.image" /> -->
+        </p>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
@@ -46,6 +83,7 @@ export default {
       message: "Welcome to Vue.js!",
       contacts: [],
       newContact: {},
+      currentContact: {},
     };
   },
   created: function () {
@@ -73,6 +111,11 @@ export default {
         .then((response) => {
           console.log(response.data);
         });
+    },
+    showContact: function (theContact) {
+      console.log("Show Contact!");
+      this.currentContact = theContact;
+      document.querySelector("#show-contact").showModal();
     },
   },
 };
