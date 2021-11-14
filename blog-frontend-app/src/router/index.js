002f9a3ba12ac13fadb7813001aefe83468c2901
Mehmet <mehmetk6158@gmail.com>
2021-11-14 11:34:29 -0500

    add other links and routers

--- blog-frontend-app/src/router/index.js
+++ blog-frontend-app/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Test from "../views/Test.vue";
+import Secondtest from "../views/Secondtest.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +11,16 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/test",
+    name: "Test",
+    component: Test,
+  },
+  {
+    path: "/secondtest",
+    name: "Secondtest",
+    component: Secondtest,
   },
   {
     path: "/about",
