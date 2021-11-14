6a5704ffbceb87234a4c730a634712908d63f1e2
Mehmet <mehmetk6158@gmail.com>
2021-11-14 12:31:11 -0500

    add signup, login ang logout auth

--- blog-frontend-app/src/router/index.js
+++ blog-frontend-app/src/router/index.js
@@ -3,7 +3,9 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Test from "../views/Test.vue";
 import Secondtest from "../views/Secondtest.vue";
-
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -21,6 +23,21 @@ const routes = [
     path: "/secondtest",
     name: "Secondtest",
     component: Secondtest,
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/logout",
+    name: "Logout",
+    component: Logout,
   },
   {
     path: "/about",
