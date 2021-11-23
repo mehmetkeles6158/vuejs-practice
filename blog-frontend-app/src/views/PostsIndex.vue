baa8b58771a1a746ef93fb9491410978e91dd73d
Mehmet <mehmetk6158@gmail.com>
2021-11-22 19:32:17 -0500

    initial commit

--- blog-frontend-app/src/views/PostsIndex.vue
+++ blog-frontend-app/src/views/PostsIndex.vue
@@ -2,7 +2,28 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <div class="row">
-      <div class="col-sm-4" v-for="post in posts" :key="post.id">
+      <button v-on:click="flipSortOrder()">Flip Sort Order</button>
+      <button v-on:click="setSortAttribute('id')">Sort by Id</button>
+      <button v-on:click="setSortAttribute('title')">Sort by Title</button>
+      Search:
+      <input type="text" v-model="searchTerm" list="titles" />
+      <datalist id="titles">
+        <option v-for="post in posts" :key="post.id">{{ post.title }}</option>
+      </datalist>
+
+      <!-- <div v-for="user in filterBy(users, 'Jack', 'name')">  -->
+      <!-- <ul>
+      <li v-for="user in orderBy(users, 'name')">
+        {{ user.name }}
+      </li> 
+    </ul> -->
+      <!-- <b>Search:</b>
+      <input type="text" v-model="searchTerm" /> -->
+      <div
+        class="col-sm-4"
+        v-for="post in orderBy(filterBy(posts, searchTerm, 'title'), sortAttribute, sortOrder)"
+        :key="post.id"
+      >
         <div class="card">
           <img v-bind:src="post.image" class="card-img-top" v-bind:alt="post.body" />
           <div class="card-body">
@@ -27,11 +48,16 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function () {
     return {
       message: "Welcome to the blog!",
       posts: [],
+      searchTerm: "",
+      sortOrder: 1,
+      sortAttribute: "title",
     };
   },
   created: function () {
@@ -44,6 +70,12 @@ export default {
         console.log(response.data);
         this.posts = response.data;
       });
+    },
+    flipSortOrder: function () {
+      this.sortOrder = this.sortOrder * -1;
+    },
+    setSortAttribute: function (attribute) {
+      this.sortAttribute = attribute;
     },
   },
 };
