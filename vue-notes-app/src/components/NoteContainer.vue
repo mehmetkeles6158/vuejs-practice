cf87eb95fe53bda63c68c0e75eed150532b8ba1a
Mehmet <mehmetk6158@gmail.com>
2021-11-22 20:08:52 -0500

    add note container and note selector to separate file and do some layers

---
+++ vue-notes-app/src/components/NoteContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="note-container">
+    <NoteSelector />
+    <div class="note-editor">
+      <p class="note-editor-info">Timestamp here...</p>
+      <textarea class="note-editor-input">
+	          First note...
+
+	          Note text here...
+	        </textarea
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import NoteSelector from "./NoteSelector.vue";
+export default {
+  components: {
+    NoteSelector,
+  },
+};
+</script>
