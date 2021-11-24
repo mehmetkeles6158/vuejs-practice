0da4dc00b093b02e3181553d4cde35bea6d796a6
Mehmet <mehmetk6158@gmail.com>
2021-11-23 22:01:57 -0500

    add own example map

--- mapbox-practice/src/views/About.vue
+++ mapbox-practice/src/views/About.vue
@@ -1,5 +1,162 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div id="map" style="width: 800px; height: 600px"></div>
   </div>
 </template>
+
+<style></style>
+
+<script>
+import mapboxgl from "mapbox-gl";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to About-Vue.js!",
+    };
+  },
+  mounted: function () {
+    this.generateMap();
+  },
+  methods: {
+    generateMap: function () {
+      mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_CODE;
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoibWVobWV0azYxNTgiLCJhIjoiY2t3YmlxM3l2MnVpOTJubmhhcTg0bnVpdSJ9._PYMxx9LYNNuT8Z9I0q5Eg";
+      (async () => {
+        const map = new mapboxgl.Map({
+          container: "map",
+          zoom: 13,
+          center: [6.58968, 45.39701],
+          pitch: 76,
+          bearing: 150,
+          style: "mapbox://styles/mapbox/satellite-streets-v11",
+          interactive: false,
+          hash: false,
+        });
+
+        // Start downloading the route data, and wait for map load to occur in parallel
+        const [pinRouteGeojson] = await Promise.all([
+          fetch("https://docs.mapbox.com/mapbox-gl-js/assets/route-pin.geojson").then((response) => response.json()),
+          map.once("load"),
+        ]);
+
+        const pinRoute = pinRouteGeojson.features[0].geometry.coordinates;
+        // Create the marker and popup that will display the elevation queries
+        const popup = new mapboxgl.Popup({ closeButton: false });
+        const marker = new mapboxgl.Marker({
+          color: "red",
+          scale: 0.8,
+          draggable: false,
+          pitchAlignment: "auto",
+          rotationAlignment: "auto",
+        })
+          .setLngLat(pinRoute[0])
+          .setPopup(popup)
+          .addTo(map)
+          .togglePopup();
+
+        // Add a line feature and layer. This feature will get updated as we progress the animation
+        map.addSource("line", {
+          type: "geojson",
+          // Line metrics is required to use the 'line-progress' property
+          lineMetrics: true,
+          data: pinRouteGeojson,
+        });
+        map.addLayer({
+          type: "line",
+          source: "line",
+          id: "line",
+          paint: {
+            "line-color": "rgba(0,0,0,0)",
+            "line-width": 5,
+          },
+          layout: {
+            "line-cap": "round",
+            "line-join": "round",
+          },
+        });
+
+        // Add some fog in the background
+        map.setFog({
+          range: [-0.5, 2],
+          color: "white",
+          "horizon-blend": 0.2,
+        });
+
+        // Add a sky layer over the horizon
+        map.addLayer({
+          id: "sky",
+          type: "sky",
+          paint: {
+            "sky-type": "atmosphere",
+            "sky-atmosphere-color": "rgba(85, 151, 210, 0.5)",
+          },
+        });
+
+        // Add terrain source, with slight exaggeration
+        map.addSource("mapbox-dem", {
+          type: "raster-dem",
+          url: "mapbox://mapbox.terrain-rgb",
+          tileSize: 512,
+          maxzoom: 14,
+        });
+        map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
+
+        await map.once("idle");
+        // The total animation duration, in milliseconds
+        const animationDuration = 20000;
+        // Use the https://turfjs.org/ library to calculate line distances and
+        // sample the line at a given percentage with the turf.along function.
+        const path = turf.lineString(pinRoute);
+        // Get the total line distance
+        const pathDistance = turf.lineDistance(path);
+        let start;
+        function frame(time) {
+          if (!start) start = time;
+          const animationPhase = (time - start) / animationDuration;
+          if (animationPhase > 1) {
+            return;
+          }
+
+          // Get the new latitude and longitude by sampling along the path
+          const alongPath = turf.along(path, pathDistance * animationPhase).geometry.coordinates;
+          const lngLat = {
+            lng: alongPath[0],
+            lat: alongPath[1],
+          };
+
+          // Sample the terrain elevation. We round to an integer value to
+          // prevent showing a lot of digits during the animation
+          const elevation = Math.floor(
+            // Do not use terrain exaggeration to get actual meter values
+            map.queryTerrainElevation(lngLat, { exaggerated: false })
+          );
+
+          // Update the popup altitude value and marker location
+          popup.setHTML("Altitude: " + elevation + "m<br/>");
+          marker.setLngLat(lngLat);
+
+          // Reduce the visible length of the line by using a line-gradient to cutoff the line
+          // animationPhase is a value between 0 and 1 that reprents the progress of the animation
+          map.setPaintProperty("line", "line-gradient", [
+            "step",
+            ["line-progress"],
+            "red",
+            animationPhase,
+            "rgba(255, 0, 0, 0)",
+          ]);
+
+          // Rotate the camera at a slightly lower speed to give some parallax effect in the background
+          const rotation = 150 - animationPhase * 40.0;
+          map.setBearing(rotation % 360);
+
+          window.requestAnimationFrame(frame);
+        }
+
+        window.requestAnimationFrame(frame);
+      })();
+    },
+  },
+};
+</script>
--- mapbox-practice/src/views/Home.vue
+++ mapbox-practice/src/views/Home.vue
@@ -7,7 +7,7 @@
 </template>
 
 <style>
-#marker {
+/* #marker {
   background-image: url("https://docs.mapbox.com/mapbox-gl-js/assets/washington-monument.jpg");
   background-size: cover;
   width: 50px;
@@ -18,7 +18,7 @@
 
 .mapboxgl-popup {
   max-width: 200px;
-}
+} */
 </style>
 
 <script>
@@ -36,38 +36,87 @@ export default {
   methods: {
     generateMap: function () {
       mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_CODE;
+
+      const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/light-v10",
+        center: [-87.62712, 41.89033],
+        zoom: 15.5,
+        pitch: 45,
+      });
+
+      function rotateCamera(timestamp) {
+        // clamp the rotation between 0 -360 degrees
+        // Divide timestamp by 100 to slow rotation to ~10 degrees / sec
+        map.rotateTo((timestamp / 100) % 360, { duration: 0 });
+        // Request the next frame of the animation.
+        requestAnimationFrame(rotateCamera);
+      }
+
+      map.on("load", () => {
+        // Start the animation.
+        rotateCamera(0);
+
+        // Add 3d buildings and remove label layers to enhance the map
+        const layers = map.getStyle().layers;
+        for (const layer of layers) {
+          if (layer.type === "symbol" && layer.layout["text-field"]) {
+            // remove text labels
+            map.removeLayer(layer.id);
+          }
+        }
+
+        map.addLayer({
+          id: "3d-buildings",
+          source: "composite",
+          "source-layer": "building",
+          filter: ["==", "extrude", "true"],
+          type: "fill-extrusion",
+          minzoom: 15,
+          paint: {
+            "fill-extrusion-color": "#aaa",
+
+            // use an 'interpolate' expression to add a smooth transition effect to the
+            // buildings as the user zooms in
+            "fill-extrusion-height": ["interpolate", ["linear"], ["zoom"], 15, 0, 15.05, ["get", "height"]],
+            "fill-extrusion-base": ["interpolate", ["linear"], ["zoom"], 15, 0, 15.05, ["get", "min_height"]],
+            "fill-extrusion-opacity": 0.6,
+          },
+        });
+      });
+
       // const map = new mapboxgl.Map({
       //   container: "map", // container ID
       //   style: "mapbox://styles/mapbox/streets-v11", // style URL
       //   center: [-74.5, 40], // starting position [lng, lat]
       //   zoom: 9, // starting zoom
       // });
-      const monument = [-77.0353, 38.8895];
-      const map = new mapboxgl.Map({
-        container: "map",
-        style: "mapbox://styles/mapbox/light-v10",
-        center: monument,
-        zoom: 15,
-      });
+      // const monument = [-77.0353, 38.8895];
+      // const map = new mapboxgl.Map({
+      //   container: "map",
+      //   style: "mapbox://styles/mapbox/light-v10",
+      //   center: monument,
+      //   zoom: 15,
+      // });
 
-      // create the popup
-      const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-        "Construction on the Washington Monument began in 1848."
-      );
+      // // create the popup
+      // const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+      //   "Construction on the Washington Monument began in 1848."
+      // );
 
-      // create DOM element for the marker
-      const el = document.createElement("div");
-      el.id = "marker";
+      // // create DOM element for the marker
+      // const el = document.createElement("div");
+      // el.id = "marker";
 
-      // create the marker
-      new mapboxgl.Marker(el)
-        .setLngLat(monument)
-        .setPopup(popup) // sets a popup on this marker
-        .addTo(map);
-      const marker1 = new mapboxgl.Marker().setLngLat([38.8893, 77.0502]).addTo(map);
+      // // create the marker
+      // new mapboxgl.Marker(el)
+      //   .setLngLat(monument)
+      //   .setPopup(popup) // sets a popup on this marker
+      //   .addTo(map);
+      // const marker1 = new mapboxgl.Marker().setLngLat([38.8893, 77.0502]).addTo(map);
 
-      // Create a default Marker, colored black, rotated 45 degrees.
-      const marker2 = new mapboxgl.Marker({ color: "black", rotation: 45 }).setLngLat([38.8875, 77.0364]).addTo(map);
+      // // Create a default Marker, colored black, rotated 45 degrees.
+      // const marker2 = new mapboxgl.Marker({ color: "black", rotation: 45 }).setLngLat([38.8875, 77.0364]).addTo(map);
     },
   },
 };
