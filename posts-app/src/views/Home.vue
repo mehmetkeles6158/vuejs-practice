3e77676f3cfa04eac173de8809854ddbd76abb0e
Mehmet <mehmetk6158@gmail.com>
2021-12-01 19:51:30 -0500

    create index action

---
+++ posts-app/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div v-for="post in posts">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      posts: [],
+    };
+  },
+  created: function () {
+    this.postsIndex();
+  },
+  methods: {
+    postsIndex: function () {
+      console.log("Posts are loading");
+      axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+        console.log(response.data);
+        this.posts = response.data;
+      });
+    },
+  },
+};
+</script>
