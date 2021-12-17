2626fc471e4d5de192009400c4bb48eaba8d3dd7
Mehmet <mehmetk6158@gmail.com>
2021-12-16 22:20:09 -0500

    call current user in user.class

--- resume_data_frontend/src/views/Home.vue
+++ resume_data_frontend/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+
+    <div v-for="student in students">
+      <h2>{{ student }}</h2>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Students' Info",
+      students: [],
+    };
+  },
+  created: function () {
+    this.studentsIndex();
+  },
+  methods: {
+    studentsIndex: function () {
+      axios.get("/students/1").then((response) => {
+        console.log(response.data);
+        this.students = response.data;
+      });
+    },
+  },
+};
 </script>
